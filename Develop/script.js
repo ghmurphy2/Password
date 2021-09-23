b4c2cd03a9007d9c06f8d40a5b26b7c9dbce954d
Gavin Murphy <gavinhmurphy@gmail.com>
2021-09-22 22:19:08 -0700

    Roughed out functions and pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+
+alert ("Would you like to generate a new password?");
+
+
+let prompt ("How many characters long yould you like your password to be?(please select between 8-126)");
+
+function parameters (){
+alert ("Would you like this password to include special characters?");
+alert ("Would you like your password to include numbers?");
+alert( "would you like you password to include capitals?")
+}
+
+
+parameters
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
