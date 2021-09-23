b525e6cae75d365799f93f067e44181912de04a1
Gavin Murphy <gavinhmurphy@gmail.com>
2021-09-22 22:29:21 -0700

    started coding true functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var characterSet = 
 // Write password to the #password input
+//add characters into data set, start with 97-122
 
 alert ("Would you like to generate a new password?");
 
 
-let prompt ("How many characters long yould you like your password to be?(please select between 8-126)");
+let prompt = ("How many characters long yould you like your password to be?(please select between 8-126)");
 
-function parameters (){
-alert ("Would you like this password to include special characters?");
-alert ("Would you like your password to include numbers?");
-alert( "would you like you password to include capitals?")
+function parameters1 (){
+return alert ("Would you like this password to include special characters?");
+}
+function parameters2 (){  
+return alert ("Would you like your password to include numbers?");
+}
+function parameters3 (){
+return alert( "would you like you password to include capitals?");
 }
 
+parameters()
 
-parameters
+function lower (){ 
+  return (Math.floor (Math.random * 26 ); 
+}
 
+function symbol(){
+  const symbols = " !@#%^&*()_+" ;
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  return 
+}
 
-  passwordText.value = password;
+//add to 97 for lower case
+
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//create seperate math.randoms for character array. push array with any characters
+//selected remeber the character number sheet. want capital? push true value into data array
